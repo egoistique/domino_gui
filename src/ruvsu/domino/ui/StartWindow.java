aba9a4cc0a212359ba8400a02439ee4c0ba6c5a9
katya <katyamit5@gmail.com>
2022-11-25 19:03:00 +0300

    Revert "мелкие доработки"
    This reverts commit 8fe187df6020a1a6a4507c55134657616c99d452.

--- src/ruvsu/domino/ui/StartWindow.java
+++ src/ruvsu/domino/ui/StartWindow.java
@@ -38,6 +38,7 @@ public class StartWindow extends JFrame{
         buttonOk.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                num = 2;
                 if(radio3.isSelected()) {
                     num = 3;
                 } else if (radio4.isSelected()) {
